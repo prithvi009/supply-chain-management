eaff9ee100f46ff016213fe440ef82b6dcc0f9c5
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-04-22 13:56:53 +0530

    addhistory and addproduct created

--- web3/contracts/SmartContract.sol
+++ web3/contracts/SmartContract.sol
@@ -28,4 +28,22 @@ contract ProductTraceability {
        emit NewProduct(productCount, _name, _manufacturer, _amount, _quantity, _weight, _sealHash, msg.sender);
        return _sealHash;
    }
+
+   function addHistory(uint256 _id, string memory _history, bytes32 _sealHash) public returns(bytes32 ){
+       require(_id > 0 && _id <= productCount, "Invalid product ID");
+       Product storage product = products[_id];
+       require(product.sealHash == _sealHash);
+       product.history.push(_history);
+       emit ProductHistory(_id, _history);
+
+       bytes32 _sealHashs = keccak256(abi.encodePacked(block.timestamp, msg.sender, block.difficulty));
+       product.sealHash = _sealHashs;
+       return _sealHashs;
+   }
+  
+   function getProduct(uint256 _id) public view returns (string memory name, string memory manufacturer, uint256 amount, uint256 quantity, uint256 weight, string[] memory history, bytes32 sealHash, address sender) {
+       require(_id > 0 && _id <= productCount, "Invalid product ID");
+       Product storage product = products[_id];
+       return (product.name, product.manufacturer, product.amount, product.quantity, product.weight, product.history, product.sealHash, product.sender);
+   }
 }
