e314e726b91d346d35ef3e93752fcd5b0622a44d
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-11-19 18:39:40 +0530

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: "0.8.18",
   networks:{
     polygon_mumbai:{
-      url : `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${POLYGON_PRIVATE_KEY}`],
+      url : `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.POLYGON_PRIVATE_KEY}`],
     }
   },
 };
